49bf9c8ec732b3e0cf69f49018bb9ca1242f633f
alexbzmn <crashius@gmail.com>
2017-09-20 23:29:15 +0200

    travis docker tests: fix dependency issues (#9352)

--- build_tools/travis/docker_test.sh
+++ build_tools/travis/docker_test.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-# This test script is mean to be executed with 'docker run' using one of the manylinux images
+# This script is meant to be executed with 'docker run' using one of the manylinux images.
+# The script employs running tests on 32bit linux image.
+# The behavior of the script is controlled by environment variables defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh \
         -O miniconda.sh
 
 MINICONDA_PATH=/home/travis/miniconda
 chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
+
+export PATH_OLD=$PATH
 export PATH=$MINICONDA_PATH/bin:$PATH
+
 conda update --yes conda
 
 conda create -n testenv --yes python=$PYTHON_VERSION pip \
@@ -16,13 +24,11 @@ conda create -n testenv --yes python=$PYTHON_VERSION pip \
 
 source activate testenv
 
-export PATH=$MINICONDA_PATH/envs/testenv/bin:$PATH
-
-rm setup.cfg
+export PATH=$MINICONDA_PATH/envs/testenv/bin:$PATH_OLD
 
 set -e
-export PATH=/opt/python/cp36-cp36m/bin:$PATH
-pip install nose pytest
+pip install numpy scipy cython nose pytest
 cd /io
+rm setup.cfg
 pip install -e .
 pytest -l sklearn
